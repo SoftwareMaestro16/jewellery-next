6d73fc1fd948a74f227801d54f586ee6956834d7
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-04-19 21:30:15 +0300

    add

--- src/app/shop/[jewelryId]/page.tsx
+++ src/app/shop/[jewelryId]/page.tsx
@@ -1,18 +1,21 @@
 import { NextPage } from 'next';
 
 type PageProps = {
-    params: Promise<{ jewelryId: string }>;
-    searchParams?: { [key: string]: string | string[] | undefined };
+  params: Promise<{ jewelryId: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
-const JewelryId: NextPage<PageProps> = async ({ params }) => {
-    const resolvedParams = await params; // Разрешаем промис
-    const { jewelryId } = resolvedParams;
-    return (
-        <>
-            <h1>Тип: {jewelryId}</h1>
-        </>
-    );
+const JewelryId: NextPage<PageProps> = async ({ params, searchParams }) => {
+  const resolvedParams = await params; // Resolve params promise
+  const resolvedSearchParams = await searchParams; // Resolve searchParams promise
+  const { jewelryId } = resolvedParams;
+
+  return (
+    <>
+      <h1>Тип: {jewelryId}</h1>
+      <pre>{JSON.stringify(resolvedSearchParams, null, 2)}</pre> {/* Optional: Display searchParams */}
+    </>
+  );
 };
 
 export default JewelryId;
