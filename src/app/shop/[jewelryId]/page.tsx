2d7054df9325afaae3ee4aeb38fa67b41a43cf81
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-04-19 21:35:55 +0300

    add

--- src/app/shop/[jewelryId]/page.tsx
+++ src/app/shop/[jewelryId]/page.tsx
@@ -4,15 +4,13 @@ type PageProps = {
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
   };
   
-  export default async function JewelryId({ params, searchParams }: PageProps) {
+  export default async function JewelryId({ params }: PageProps) {
     const resolvedParams = await params; // Resolve params promise
-    const resolvedSearchParams = await searchParams; // Resolve searchParams promise
+    // const resolvedSearchParams = await searchParams; 
     const { jewelryId } = resolvedParams;
   
     return (
       <>
-        <h1>Тип: {jewelryId}</h1>
-        <pre>{JSON.stringify(resolvedSearchParams, null, 2)}</pre> {/* Optional: Display searchParams */}
       </>
     );
   }
