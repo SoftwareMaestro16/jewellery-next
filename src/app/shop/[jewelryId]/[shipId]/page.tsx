bd3f3130b826e3eb273e1dbce3b036a4f4727752
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-04-19 21:06:54 +0300

    add

--- src/app/shop/[jewelryId]/[shipId]/page.tsx
+++ src/app/shop/[jewelryId]/[shipId]/page.tsx
@@ -1,17 +1,16 @@
 type ShipIdPageProps = {
-    params: {
-      jewelryId: string;
-      shipId: string;
-    };
+  params: {
+    jewelryId: string;
+    shipId: string;
+  };
 };
-  
+
 export default function ShipId({ params }: ShipIdPageProps) {
-    const { jewelryId, shipId } = params;
-  
-    return (
-      <div>
-        <h1>Type: {jewelryId}</h1>
-        <h2>Name (slug): {shipId}</h2>
-      </div>
-    );
+  const { jewelryId, shipId } = params;
+  return (
+    <div>
+      <h1>Type: {jewelryId}</h1>
+      <h2>Name (slug): {shipId}</h2>
+    </div>
+  );
 }
