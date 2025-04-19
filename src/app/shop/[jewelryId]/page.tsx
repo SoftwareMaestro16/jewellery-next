a570ff9df878b1e9567e33c1413429a6c6effb3f
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-04-19 21:34:03 +0300

    add

--- src/app/shop/[jewelryId]/page.tsx
+++ src/app/shop/[jewelryId]/page.tsx
@@ -1,21 +1,18 @@
-import { NextPage } from 'next';
 
 type PageProps = {
-  params: Promise<{ jewelryId: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-};
-
-const JewelryId: NextPage<PageProps> = async ({ params, searchParams }) => {
-  const resolvedParams = await params; // Resolve params promise
-  const resolvedSearchParams = await searchParams; // Resolve searchParams promise
-  const { jewelryId } = resolvedParams;
-
-  return (
-    <>
-      <h1>Тип: {jewelryId}</h1>
-      <pre>{JSON.stringify(resolvedSearchParams, null, 2)}</pre> {/* Optional: Display searchParams */}
-    </>
-  );
-};
-
-export default JewelryId;
+    params: Promise<{ jewelryId: string }>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  };
+  
+  export default async function JewelryId({ params, searchParams }: PageProps) {
+    const resolvedParams = await params; // Resolve params promise
+    const resolvedSearchParams = await searchParams; // Resolve searchParams promise
+    const { jewelryId } = resolvedParams;
+  
+    return (
+      <>
+        <h1>Тип: {jewelryId}</h1>
+        <pre>{JSON.stringify(resolvedSearchParams, null, 2)}</pre> {/* Optional: Display searchParams */}
+      </>
+    );
+  }
